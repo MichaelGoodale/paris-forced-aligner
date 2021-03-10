c8cc3396ece049690bc91fce8123e9bf0b3e5140
Michael Goodale <michael.goodale@mail.mcgill.ca>
2021-03-10 09:34:07 +0100

    Fixed issue with training

--- train_english.sh
+++ train_english.sh
@@ -1,3 +1,5 @@
 wget https://www.openslr.org/resources/12/train-clean-100.tar.gz
 tar xvzf train-clean-100.tar.gz
-paris_forced_trainer --corpus_path LibriSpeech/ --wav2vec_model small --download-wav2vec --learning_rate 0.0001 --batch_size 8 --unfreeze_after 1200 --output_model_every 400 --zero_lambda_until 3200 --n_steps 4000 --accumulate_steps=2 --gpu
+rm train-clean-100.tar.gz
+paris_forced_trainer --corpus_path LibriSpeech/ --wav2vec_model small --download-wav2vec --learning_rate 0.0001 --batch_size 4 --unfreeze_after 1200 --output_model_every 400 --zero_lambda_until 3200 --n_steps 4000 --accumulate_steps=2 --gpu
+
