8631aab7dfd7ebefc0deb63b8c2c73f6c4fd028e
Michael Goodale <michael.goodale@mail.mcgill.ca>
2021-05-27 23:51:12 +0200

    Fixed weighting of blankless features for real

--- paris_forced_aligner/ipa_data.py
+++ paris_forced_aligner/ipa_data.py
@@ -280,7 +280,7 @@ def multilabel_ctc_log_prob(c: Dict[str, Tensor], device='cpu') -> Tensor:
         char_probs = [] 
         for i, feature in enumerate(ORDERING):
             component_prob = c[feature][:, :, vec[i]]
-            if vec[i] == 1 and feature in BLANKLESS_FEATURES: #(component blank):
+            if vec[i] == 1 and feature not in BLANKLESS_FEATURES: #(component blank):
                 component_prob = 0*component_prob
             char_probs.append(component_prob)
         char_probs = torch.stack(char_probs, dim=0)
