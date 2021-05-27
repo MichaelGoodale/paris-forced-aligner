90852b7c28a664563b57cb88a9e04c02132adf22
Michael Goodale <michael.goodale@mail.mcgill.ca>
2021-05-27 22:14:43 +0200

    Shuffle tsv data and removed weird unicode char from affricates

--- paris_forced_aligner/corpus/tsv_corpus.py
+++ paris_forced_aligner/corpus/tsv_corpus.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import random
 
 from paris_forced_aligner.pronunciation import LibrispeechDictionary, AudioFile, OutOfVocabularyException, PronunciationDictionary
 from paris_forced_aligner.phonological import Utterance, Silence, Word, Phone
@@ -8,6 +9,7 @@ from paris_forced_aligner.corpus import CorpusClass
 class TSVCorpus(CorpusClass):
     def __init__(self, corpus_path: str, pronunciation_dictionary: PronunciationDictionary = None, delimiter='\t', path_prefix=''):
         super().__init__(corpus_path, pronunciation_dictionary)
+        self.random = random.Random(1337)
         self.all_files = []
         with open(corpus_path) as f:
             csv_file = csv.DictReader(f, delimiter=delimiter)
@@ -17,7 +19,7 @@ class TSVCorpus(CorpusClass):
     def extract_files(self, return_gold_labels):
         if return_gold_labels:
             raise NotImplementedError("TSVCorpus does not support gold standard phonemic labels")
-
+        self.random.shuffle(self.all_files)
         for filename, transcription in self.all_files:
             try:
                 yield AudioFile(filename, transcription, self.pronunciation_dictionary)
